f9c07f790a8468027b2c3eed4ba8cb17f5929f2d
Kauê Alcantara Rocha <118750523+Kaueroch@users.noreply.github.com>
2024-01-13 15:21:40 -0300

    Add files via upload
    comecei a usar o eclipse.

---
+++ codigo.java
@@ -0,0 +1,14 @@
+
+public class codigo {
+	public static void main(String[] args) {
+     for(int x = 100;x  <=200;x++ ) {
+    	 if(x%2 == 1) {
+    		 System.out.println(x);
+    	 }
+     }
+	}
+}
+/*******************************************************************************
+ * 28) Faça um programa que gera e escreve os números ímpares dos números lidos
+ * entre 100 e 200.
+ *******************************************************************************/
